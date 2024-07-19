303e8686977c92651b98b665727ec95c616a524a
Piotrek <pdrozd20@wp.pl>
2024-07-19 14:54:04 +0100

    zadanie 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Piotrek";
+var sex = "mezczyzna";
+var age = 35;
+
+if (sex == "kobieta" && age < 30)
+{
+    if (name == "Ewa" && age == 30)
+    {
+        Console.WriteLine("Ewa lat 30");
+    }
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (sex == "kobieta" && age < 30)
+
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+else if(sex == "mezczyzna" && age < 18) { Console.WriteLine("Niepelnoletni mezczyzna"); }
+{
+
+}
