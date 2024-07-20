a0be414336c32e424fff84916fcea2e9e20a9d1e
Piotrek <pdrozd20@wp.pl>
2024-07-20 12:09:04 +0100

    Zadanie 4.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,21 +1,16 @@
-﻿var name = "Piotrek";
+﻿using System.ComponentModel.Design;
+
+var name = "Piotrek";
 var sex = "mezczyzna";
 var age = 35;
 
-if (sex == "kobieta" && age < 30)
+if (!(sex == "kobieta" && age < 30))
 {
-    if (name == "Ewa" && age == 30)
-    {
-        Console.WriteLine("Ewa lat 30");
-    }
     Console.WriteLine("Kobieta ponizej 30 lat");
 }
-else if (sex == "kobieta" && age < 30)
-
+else if (name == "Ewa" && age == 30)
 {
-    Console.WriteLine("Niepelnoletni mezczyzna");
+    Console.WriteLine("Ewa, lat 30");
 }
-else if(sex == "mezczyzna" && age < 18) { Console.WriteLine("Niepelnoletni mezczyzna"); }
-{
-
-}
+else if (sex == "mezczyzna" && age < 18)
+{ Console.WriteLine("Niepelnoletni mezczyzna"); }
